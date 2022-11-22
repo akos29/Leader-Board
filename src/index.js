4d4451f1d7942e504ca9e33122da4a9de4bfe0ea
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2022-11-22 22:37:50 +0200

    Fix eslint error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import _ from 'lodash';
 import './style.css';
 import generateScore from './modules/generateScore.js';
 
 function component() {
-  generateScore('Abebe',1)
+  generateScore('Abebe', 1);
 }
 
 component();
