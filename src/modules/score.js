766421666587f575566a98d515666fd7b1fc2fdf
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2022-11-23 23:14:38 +0200

    Add functionality to generate from my game

--- src/modules/score.js
+++ src/modules/score.js
@@ -1,11 +1,26 @@
-class Score {
-    constructor(){
-        this.arr= localStorage.getItem() || [];
-    }
-
-    addScore(name , score){
-        // this.arr.push(name,score);
-    }
-
-    
-}
+export default class Score {
+        static BASE_URL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/wzBdoZ3ft7zrPr0kMrEE/scores`;
+      
+        static addScore = async ({ user, score }) => {
+          const game = {
+            user,
+            score,
+          };
+      
+          const data = await fetch(this.BASE_URL, {
+            method: 'POST',
+            body: JSON.stringify(game),
+            headers: {
+              'Content-type': 'application/json; charset=UTF-8',
+            },
+          });
+          const response = await data.json();
+          return response;
+        };
+      
+        static getScore = async () => {
+          const response = await fetch(this.BASE_URL);
+          const games = await response.json();
+          return games.result;
+        };
+      }
